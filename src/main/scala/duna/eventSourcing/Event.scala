724b870a99e6ce71f62dc60d00716063d4708a83
Victoria Zakharova <garrynsk@gmail.com>
2018-03-18 22:15:24 +0300

    Remove Event and Computation

--- src/main/scala/duna/eventSourcing/Event.scala
+++
@@ -1,9 +0,0 @@
-
-package duna
-package eventSourcing
-
-import duna.kernel.Computation
-
-trait IEvent[@specialized(Short, Char, Int, Float, Long, Double, AnyRef) A]
-case class NoEvent[@specialized(Short, Char, Int, Float, Long, Double, AnyRef) A]() extends IEvent[A]
-case class Event[Index, @specialized(Short, Char, Int, Float, Long, Double, AnyRef) A](index: Index, computation: A) extends IEvent[A]
